5ea1a7ba74432eea34a2b5799f63018f03d03393
tselishev <82764755+tselishev@users.noreply.github.com>
2021-05-25 20:22:13 +0500

    доавлены тесты на Мастеркард, Маестро, Виза, Мир

--- src/test/kotlin/Task1KtTest.kt
+++ src/test/kotlin/Task1KtTest.kt
@@ -17,5 +17,56 @@ class Task1KtTest {
         )
         assertEquals(00.00, result, 0.0001)
     }
+    @Test
+    fun masterCardCalc_shouldNotAddDiscount() {
+        val card = "MasterCard"
+        val transactionMonth = 12000
+        val transaction = 500
 
+        val result = comission(
+            card = "MasterCard",
+            transactionMonth = transactionMonth,
+            transaction = transaction
+        )
+        assertEquals(00.00, result, 0.0001)
+    }
+    @Test
+    fun maestroCalc_shouldNotAddDiscount() {
+        val card = "Maestro"
+        val transactionMonth = 10000
+        val transaction = 1000
+
+        val result = comission(
+            card = "Maestro",
+            transactionMonth = transactionMonth,
+            transaction = transaction
+        )
+        assertEquals(00.00, result, 0.0001)
+    }
+    @Test
+    fun visaCalc_shouldNotAddDiscount() {
+        val card = "Visa"
+        val transactionMonth = 10000
+        val transaction = 10000
+
+        val result = comission(
+            card = "Visa",
+            transactionMonth = transactionMonth,
+            transaction = transaction
+        )
+        assertEquals(75.00, result, 0.0001)
+    }
+    @Test
+    fun mirCalc_shouldNotAddDiscount() {
+        val card = "Мир"
+        val transactionMonth = 11000
+        val transaction = 11000
+
+        val result = comission(
+            card = "Мир",
+            transactionMonth = transactionMonth,
+            transaction = transaction
+        )
+        assertEquals(82.50, result, 0.0001)
+    }
 }
